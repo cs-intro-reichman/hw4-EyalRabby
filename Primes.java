13ca9f480d1f207ef5bc5b23d6ca053326d2bfd6
EyalRabby <eyal.rabby@post.runi.ac.il>
2024-12-05 14:14:21 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -5,27 +5,21 @@ public class Primes {
     }
 
     public static void sieveOfEratosthenes(int limit) {
-        // Create a boolean array "prime[0..limit]" and initialize all entries as true.
-        // A value in prime[i] will be false if 'i' is Not a prime, true otherwise.
         boolean[] prime = new boolean[limit + 1];
         for (int i = 0; i <= limit; i++) {
             prime[i] = true;
         }
 
-        prime[0] = prime[1] = false; // 0 and 1 are not prime numbers
+        prime[0] = prime[1] = false;
 
-        // Start with the first prime number, which is 2
         for (int p = 2; p * p <= limit; p++) {
-            // If prime[p] is still true, then it's a prime number
             if (prime[p] == true) {
-                // Mark all multiples of p as false (not prime)
                 for (int i = p * p; i <= limit; i += p) {
                     prime[i] = false;
                 }
             }
         }
-
-        // Print all prime numbers
+        
         int PrimeAmount = 0;
         System.out.println("Prime numbers up to " + limit + ":");
         for (int p = 2; p <= limit; p++) {
